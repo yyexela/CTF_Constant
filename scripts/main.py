792445588d597f1be7b43ef86bfbb6076df207c2
Alexey Y <yyexela@disroot.org>
2025-03-20 15:42:00 -0700

    Random search with yaml file works

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 # Standard imports
 import os
 import sys
+import yaml
 import argparse
 import numpy as np
 from pathlib import Path
@@ -13,18 +14,19 @@ import helpers
 def main(args=None):
     # Load training and testing matrix
     train_mat, test_mat = helpers.load_dataset_raw(args.dataset, args.matrix_id)
-    data_dim = train_mat.shape[0]
-    train_len, test_len = train_mat.shape[1], test_mat.shape[1]
 
     # Generate predictions based on search type
     if args.search_type == "avg":
         # Predicted matrix is average of training matrix
         pred_mat = np.average(train_mat, axis=1)
         pred_mat = np.reshape(pred_mat, (-1,1))
-        pred_mat = np.tile(pred_mat, (1,test_len))
+        pred_mat = np.tile(pred_mat, (1,test_mat.shape[1]))
     elif args.search_type == "zero":
         # Predicted matrix is just zeros
         pred_mat = np.zeros_like(test_mat)
+    elif args.search_type == "constant":
+        # Predicted matrix is just a constant
+        pred_mat = np.ones_like(test_mat)*args.constant
     elif args.search_type == "random":
         # Predicted matrix is what evaluates best on testing set from random search
 
@@ -42,11 +44,11 @@ def main(args=None):
 
         # Set up hyperparameters
         if args.random_distribution == "uniform":
-            hyperparams = np.linspace(args.random_lower_bound,
+            hyperparams = np.random.uniform(args.random_lower_bound,
                                       args.random_upper_bound,
                                       args.random_n_values)
         elif args.random_distribution == "log":
-            hyperparams = np.linspace(args.random_lower_bound,
+            hyperparams = helpers.loguniform(args.random_lower_bound,
                                       args.random_upper_bound,
                                       args.random_n_values)
         else:
@@ -65,33 +67,55 @@ def main(args=None):
                 max_hyperparameter = hyperparam
             
         # Use best scoring value
-        print(f"Best hyperparameter was {max_hyperparameter:0.2f}")
+        print(f"Best hyperparameter was {max_hyperparameter}")
+        print()
         pred_mat = np.ones_like(test_mat)*max_hyperparameter
     else:
-        raise Exception(f"Search type {args.search_type} is not valid. Use one of \"avg\", \"zero\", or \"random\".")
+        raise Exception(f"Search type {args.search_type} is not valid. Use one of \"avg\", \"zero\", \"constant\", or \"random\".")
 
     # Evaluate predicted matrix
     score = helpers.scoring2(test_mat, pred_mat)
     print(f"Final score using method \"{args.search_type}\" is {score:0.2f}")
 
 if __name__ == '__main__':
-    # Config file, if used
-
-    # Hyperparameters for running this code
     parser = argparse.ArgumentParser()
+
+    # Config file, if used populate the below arguments automatically
+    parser.add_argument("--config", type=str, default=None, help="YAML configuration file.")
 
     # Dataset selection
     parser.add_argument('--dataset', type=str, default=None, help="Dataset to use, either \"ODE_Lorenz\" or \"PDE_KS\".")
     parser.add_argument('--matrix_id', type=str, default=None, help="Integer value for matrix to use from the provided dataset, a value from 1 to 10 inclusive.")
 
     # Search type
-    parser.add_argument('--search_type', type=str, default=None, help="Which type of model to use, choose from \"avg\", \"zero\", or \"random\"")
+    parser.add_argument('--search_type', type=str, default=None, help="Which type of model to use, choose from \"avg\", \"zero\", \"constant\", or \"random\"")
 
     # Random search options
     parser.add_argument('--random_lower_bound', type=float, default=None, help="When \"--search_type\" is \"rand\", provide the lower bound to search over.")
     parser.add_argument('--random_upper_bound', type=float, default=None, help="When \"--search_type\" is \"rand\", provide the upper bound to search over.")
     parser.add_argument('--random_n_values', type=int, default=None, help="When \"--search_type\" is \"rand\", provide the number of hyperparameters to search over.")
-    parser.add_argument('--random_distribution', type=str, default=None, help="When \"--search_type\" is \"rand\", Specify if we're searching over \"uniform\" or \"log\" distribution.")
+    parser.add_argument('--random_distribution', type=str, default=None, help="When \"--search_type\" is \"rand\", specify if we're searching over \"uniform\" or \"log\" distribution.")
+    parser.add_argument('--random_seed', type=float, default=None, help="When \"--search_type\" is \"rand\", specify the seed for numpy for reproducability.")
     args = parser.parse_args()
+
+    # Try opening config
+    try:
+        with open(args.config, 'r') as file:
+            data = yaml.safe_load(file)
+        print(f"Using provided config file ({args.config}).")
+
+        # Update `args` with yaml configuration
+        for key, value in data.items():
+            setattr(args, key, value)
+    except FileNotFoundError:
+        print(f"The file {args.config} was not found.")
+    except yaml.YAMLError as e:
+        print(f"Error parsing the YAML file: {e}")
+
+    # Print configuration
+    helpers.print_dictionary(vars(args), "Configuration values:", "")
+
+    # Set seed
+    np.random.seed(args.random_seed)
 
     main(args)
--- src/helpers.py
+++ src/helpers.py
@@ -2,17 +2,43 @@ import os
 import torch
 import numpy as np
 from tqdm import tqdm
+from pathlib import Path
 from scipy.io import loadmat
 import torch.utils.data as data
 
+def print_dictionary(hp_dict: dict[str, str], pre_text: str = None, post_text: str = None) -> None:
+    """
+    Print given dictionary
+
+    `hp_dict`: dictionary dictionary to print key and values for  
+    `pre-text`: text to print before dictionary  
+    `post-text`: text to print after dictionary  
+
+    Returns: `None`
+    """
+    if pre_text is not None:
+        print(pre_text)
+    for key in hp_dict.keys():
+        print(f"{key}: {hp_dict[key]}")
+    if post_text is not None:
+        print(post_text)
+
+    return None
+
 def load_dataset_raw(dataset, matrix_id):
     """
-    Load original unprocessed dataset (both training and testing matrices)
+    Load original unprocessed dataset (both training and testing matrices)  
+
+    `dataset`: Name of dataset (either \"ODE_Lorenz\", or \"PDE_KS\")  
+    `matrix_id`: An integer as a string, from 1 to 10 inclusive  
+
+    Returns: (`train_mat`, `test_mat`)  
+    The training and testing matrices
     """
     if dataset in ["ODE_Lorenz", "PDE_KS"]:
-        train_mat = loadmat(f'../../Datasets/{dataset}/X{matrix_id}train.mat')
+        train_mat = loadmat(str(Path(os.path.abspath(__file__)).parent.parent.parent.parent.absolute() / "Datasets" / dataset / f"X{matrix_id}train.mat"))
         train_mat = train_mat[list(train_mat.keys())[-1]]
-        test_mat = loadmat(f'../../Datasets/{dataset}/X{matrix_id}test.mat')
+        test_mat = loadmat(str(Path(os.path.abspath(__file__)).parent.parent.parent.parent.absolute() / "Datasets" / dataset / f"X{matrix_id}test.mat"))
         test_mat = test_mat[list(test_mat.keys())[-1]]
 
         train_mat = torch.Tensor(train_mat.astype(np.float32))
@@ -21,9 +47,37 @@ def load_dataset_raw(dataset, matrix_id):
         raise Exception(f"Timeseries dataset {dataset} not found")
     return train_mat, test_mat
 
-# SCORING FOR LEAST-SQUARE RECONSTRUCTION
 def scoring2(truth, prediction):
-    '''produce reconstruction fit score.'''
+    """
+    Scoring for least-square reconstruction  
+
+    `truth`: Ground-truth matrix  
+    `prediction`: Predicted matrix  
+
+    Returns:  
+    Floating point value as a score  
+    """
     Est = np.linalg.norm(truth-prediction,2)/np.linalg.norm(truth,2)
     E1 = 100*(1-Est)
     return E1
+
+def loguniform(low=0, high=1, size=None):
+    """
+    Generate values from a log-uniform distribution  
+
+    `low`: float or array_like of floats, optional  
+    Lower boundary of the output interval. All values generated will be greater than or equal to low. The default value is 0.  
+
+    `high`: float or array_like of floats  
+    Upper boundary of the output interval. All values generated will be less than or equal to high. The high limit may be included in the returned array of floats due to floating-point rounding in the equation low + (high-low) * random_sample(). The default value is 1.0.  
+
+    `size`: int or tuple of ints, optional  
+    Output shape. If the given shape is, e.g., (m, n, k), then m * n * k samples are drawn. If size is None (default), a single value is returned if low and high are both scalars. Otherwise, np.broadcast(low, high).size samples are drawn.  
+
+    Returns:  
+    `out`: ndarray or scalar  
+    Drawn samples from the parameterized log-uniform distribution.  
+    """
+    if low <= 0.0 : raise Exception("Lower bound has to be > 0.0")
+    values = np.exp(np.random.uniform(np.log(low), np.log(high), size))
+    return values
